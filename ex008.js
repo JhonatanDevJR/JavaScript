bc052f0e151fc76b34c42432fca350c942e5a847
JhonatanDevJR <hjhonatan90@gmail.com>
2024-09-05 23:55:24 -0300

    att

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel= 58
+console.log (`A velocidade do seu carro é: ${vel} km/h`)
+if(vel> 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+} else(vel<60)
+console.log (`Diriga sempre utilizando o cinto de segurança!`)
+
