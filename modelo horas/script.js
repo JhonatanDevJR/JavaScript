2e020a4d348fba197c358011e8aed8765025291a
JhonatanDevJR <hjhonatan90@gmail.com>
2024-09-10 23:42:22 -0300

    att

--- modelo horas/script.js
+++ modelo horas/script.js
@@ -8,14 +8,17 @@ function carregar() {
 if (hora>= 0 && hora<12) {
     img.src = 'manhã.png'
     document.body.style.background= 'rgb(204, 187, 93)'
+    msg.innerHTML=`Agora são ${hora} horas. Tenha um bom dia!☕`
     //Bom dia!
 } else if (hora>=12 && hora<18) {
     img.src= 'tarde.png'
     document.body.style.background= 'rgb(168, 129, 28)'
+    msg.innerHTML=`Agora são ${hora} horas. Tenha uma boa tarde!🌄`
     //Boa tarde!
 } else {
      img.src= 'noite.png'
      document.body.style.background= 'rgb(75, 74, 74)'
+     msg.innerHTML=`Agora são ${hora} horas. Tenha uma boa noite!😴`
     //Boa noie!
 }}
 
